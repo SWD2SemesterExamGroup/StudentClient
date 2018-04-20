43ce57743b57f12d9c28cb65e8c162ea23dcfd36
Troels H. Jensen <lord_ni@hotmail.com>
2018-04-20 20:27:40 +0200

    Test adjusted but not followed

--- src/test/java/dk/kea/student/artifacts/Controllers/StudentControllerTest.java
+++ src/test/java/dk/kea/student/artifacts/Controllers/StudentControllerTest.java
@@ -5,6 +5,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
+
+/**
+ * Help: https://dzone.com/articles/unit-and-integration-tests-in-spring-boot
+ */
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,7 +21,18 @@ public class StudentControllerTest
         // Assign
         StudentController _controller = new StudentController();
         // Act
-        String result = _controller.home();
+        String result = _controller.home(new ConcurrentModel());
+        // Assert
+        Assert.assertNotNull(result);
+        Assert.assertEquals(result, "index");
+    }
+
+    @Test
+    public void attendanceKeyPostTest() {
+        // Assign
+        StudentController _controller = new StudentController();
+        // Act
+        String result = _controller.attendanceKey(1, "", new ConcurrentModel());
         // Assert
         Assert.assertNotNull(result);
         Assert.assertEquals(result, "index");
